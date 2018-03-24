8da1980afe9df31f245bf5a00e41fc3cd20a2999
Bryan Mytko <bryanmytko@gmail.com>
2018-03-24 19:09:02 -0400

    Add basic structure styling;

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 
 const App = () => (
-  <div>
+  <div className="mainContainer">
     <Header />
     <Main />
   </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './Header.scss';
+
 const Header = () => (
   <header>
     <nav>
