8256872c280802759938dc9e32c4113255ebf10b
Bryan Mytko <bryanmytko@gmail.com>
2018-03-24 20:08:41 -0400

    Add Logo to Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,11 @@ import './Header.scss';
 
 const Header = () => (
   <header>
+    <section className="logo">
+      <div className="logo__container">
+        <h1>Rubik&#8217;s Cube Timer</h1>
+      </div>
+    </section>
     <nav>
       <ul>
         <li><Link to="/" href="/">Timer</Link></li>
